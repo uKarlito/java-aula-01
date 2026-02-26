af66e8498524a7d5b2ccc5be68c39f79e50bd31a
uKarlito <dedepimpu@gmail.com>
2026-02-25 21:33:03 -0300

    reiniciando projeto

--- api-rest/src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.api_rest.repository;
-
-import br.com.fiap.api_rest.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
-}
